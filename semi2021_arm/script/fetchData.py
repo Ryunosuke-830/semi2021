b9f6740dedfc18a535663e6d5dcd9d373498468e
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2021-12-08 15:12:59 +0900

    Refactor the haman teleop node

--- semi2021_arm/script/fetchData.py
+++ semi2021_arm/script/fetchData.py
@@ -17,88 +17,75 @@ global baseY
 baseX = 640/2.0
 baseY = 480/2.0
 
-class Position:
+class HumanPoseTeleop:
     def __init__(self):
         self.x = baseX
         self.y = baseY
         self.z = 0.0
 
+        self.human_pose_sub = rospy.Subscriber('/edgetpu_human_pose_estimator/output/poses', PeoplePoseArray, self.callback)
+        self.cmd_vel_pub = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=10)
+        self.takeoff_pub = rospy.Publisher('/tello/takeoff', Empty, queue_size=1)
 
-def callback(msg):
-    right_wrist = (False, 0)
-    right_shoulder = (False, 0)
-    left_wrist = (False,0)
-    left_shoulder = (False,0)
+    def callback(self, msg):
+        right_wrist = (False, 0)
+        right_shoulder = (False, 0)
+        left_wrist = (False,0)
+        left_shoulder = (False,0)
+
+        if msg.poses:
+            poses = msg.poses
+            limb_names = poses[0].limb_names
+            pose = poses[0].poses
+            for i,item in enumerate(limb_names):
+                if item == 'right wrist':
+                    right_wrist = (True,i)
+                elif item == 'right shoulder':
+                    right_shoulder = (True,i)
+                elif item == 'left wrist':
+                    left_wrist = (True,i)
+                elif item == 'left shoulder':
+                    left_shoulder = (True,i)
+
+            right_shoulder_pos = Position()
+            right_wrist_pos = Position()
+            left_shoulder_pos = Position()
+            left_wrist_pos = Position()
+
+            if right_wrist[0]:
+                right_wrist_pos = pose[right_wrist[1]].position
+            if left_wrist[0]:
+                left_wrist_pos = pose[left_wrist[1]].position
+            if right_shoulder[0]:
+                right_shoulder_pos = pose[right_shoulder[1]].position
+            if left_shoulder[0]:
+                left_shoulder_pos = pose[left_shoulder[1]].position
 
 
-    if msg.poses:
-        poses = msg.poses
-        limb_names = poses[0].limb_names
-        pose = poses[0].poses
-        for i,item in enumerate(limb_names):
-            if item == 'right wrist':
-                right_wrist = (True,i)
-            elif item == 'right shoulder':
-                right_shoulder = (True,i)
-            elif item == 'left wrist':
-                left_wrist = (True,i)
-            elif item == 'left shoulder':
-                left_shoulder = (True,i)
-        
-        right_shoulder_pos = Position()
-        right_wrist_pos = Position()
-        left_shoulder_pos = Position()
-        left_wrist_pos = Position()
+            # TODO: implement takeoff / landing
+            # if right_wrist_pos.y > right_shoulder_pos.y:
+            #     self.takeoff_pub.publish()
 
-        if right_wrist[0]:
-            right_wrist_pos = pose[right_wrist[1]].position
-        if left_wrist[0]:
-            left_wrist_pos = pose[left_wrist[1]].position
-        if right_shoulder[0]:
-            right_shoulder_pos = pose[right_shoulder[1]].position
-        if left_shoulder[0]:
-            left_shoulder_pos = pose[left_shoulder[1]].position
-        
-        controlX = right_wrist_pos.x - baseX
-        controlY = -(right_wrist_pos.y - baseY)
-        
+            controlX = right_wrist_pos.x - baseX
+            controlY = -(right_wrist_pos.y - baseY)
 
-        '''
-        if right_wrist_pos.y > right_shoulder_pos.y:
-            pub = rospy.Publisher('/tello/takeoff', Empty, queue_size=1)
-            pub.publish()
+            tw = Twist()
+            tw.linear.y = controlX/200.0
+            tw.linear.z = controlY/200.0
+            if left_wrist_pos.y <= left_shoulder_pos.y:
+                tw.linear.y = 0.0
+                tw.linear.z = 0.0
 
-        if left_wrist_pos.y > left_shoulder_pos.y:
-            pub = rospy.Publisher('tello/cmd_vel', Twist, queue_size=10)
-            msg = Twist()
-            msg.linear.y = 0.5
-            pub.publish()
-        '''
-        str1 = "right: x: {}, y: {} ".format(controlX,controlY)
-        str2 = "left: wristY={}, shoulderY={}".format(left_wrist_pos.y, left_shoulder_pos.y)
-        pub = rospy.Publisher('chatter', String, queue_size=10)
-        str = str1+str2
-        rospy.loginfo(str1)
-        pub.publish(str1)
-        
-        '''time.sleep(1.0)
-        pub1 = rospy.Publisher('tello/cmd_vel', Twist, queue_size=10)
-        tw = Twist()
-        tw.linear.y = controlX/200.0
-        tw.linear.z = controlY/200.0
-        if(left_wrist_pos.y <= left_shoulder_pos.y):
-            tw.linear.y = 0.0
-            tw.linear.z = 0.0
+            self.cmd_vel_pub.publish(tw)
 
-        pub1.publish(tw)
-        time.sleep(1.0)'''
-    
-
+            str1 = "right: x: {}, y: {} ".format(controlX,controlY)
+            str2 = "left: wristY={}, shoulderY={}".format(left_wrist_pos.y, left_shoulder_pos.y)
+            rospy.loginfo_throttle(0.5, str1 + str2)
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('controler', anonymous=True)
-        rospy.Subscriber('/edgetpu_human_pose_estimator/output/poses', PeoplePoseArray, callback)
+        rospy.init_node('human_pose_teleop', anonymous=True)
+        teleop_node = HumanPoseTeleop()
         rospy.spin()
-        
+
     except rospy.ROSInterruptException: pass
